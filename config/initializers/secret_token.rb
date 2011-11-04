1651ac3ae3492c0df63c7595c3c7d8cd02dcd114
urgm <meguru1@hotmail.com>
2011-11-04 09:37:31 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HorseRace::Application.config.secret_token = '8b95802ea06af18a74a5f24b8051fc6582f693e6a8000deeb41fa9f771feac3876ede81648334e91bc6e4bb4eda998b24a869932f14c7297cc7549a1ea2e79ec'
