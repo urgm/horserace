8251e8b78ee5b99ae9d5ea0439f51ea27add5626
urgm <meguru1@hotmail.com>
2011-11-08 18:30:09 +0900

    ・toppage(仮)コントローラーの修正 ・出馬表ページの作成

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -6,6 +6,23 @@ class ToppageController < ApplicationController
   end
   
   def result
-    @msg = '結果'
+    @msg = 'レース結果'
   end
+  
+  def program
+    @msg = '出馬表'
+  end
+  
+  #def add
+  #  @msg = '予想、買い目入力'
+  #end
+  
+  #def edit
+  #  @msg = '予想、買い目編集'
+  #end
+  
+  #削除って必要？要検討。
+  #def delete
+  # @msg = '予想、買い目削除'
+  #end
 end
