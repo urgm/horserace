fffe50d2c58d46142b38419c9d7359c4ed068b53
urgm <meguru1@hotmail.com>
2012-02-28 07:05:52 +0900

    nokogiriでの取り込み処理を取りあえず最小に修正

--- app/models/ProgramUtils.rb
+++ app/models/ProgramUtils.rb
@@ -9,23 +9,23 @@ class ProgramUtils
     
     trs = doc.search("//table[@class='race_table_old nk_tb_common']//tr[@class='bml1']")
     
-    programs = []
-    
-    trs.each do |tr|
-      tds = tr.xpath("td")
-      
-      program = Program.new
-
-      program.position_no = tds[0].text  
-      program.horse_no = tds[1].text
-      program.horse_name = tds[2].search("div//a").text
-      # sexAge = tds[3].text
-      program.weight = tds[4].text
-      program.jockey = tds[5].text
-      
-      programs << program
-    end
-
-    return programs
+//    programs = []
+//    
+//    trs.each do |tr|
+//      tds = tr.xpath("td")
+//      
+//      program = Program.new
+//
+//      program.position_no = tds[0].text  
+//      program.horse_no = tds[1].text
+//      program.horse_name = tds[2].search("div//a").text
+//      # sexAge = tds[3].text
+//      program.weight = tds[4].text
+//      program.jockey = tds[5].text
+//      
+//      programs << program
+//    end
+//
+//    return programs
   end
 end
